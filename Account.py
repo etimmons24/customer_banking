f7dc68ea61a778317b711014b2bc3a4118e0b3e6
etimmons24 <ezratimmonsjr@gmail.com>
2024-03-29 15:46:07 -0400

    Added code for project files

--- Account.py
+++ Account.py
@@ -6,10 +6,18 @@ class Account:
         self.balance = balance
         self.interest = interest
 
+    def get_balance(self):
+        """This method gets the balance of the account"""
+        return self.balance
+    
     # This method sets the balance of the account.
     def set_balance(self, balance):
         """Sets the balance for the for the account"""
         self.balance = balance
+
+    def get_interest(self):
+        """This method gets the interest gained for the account"""
+        return self.interest
 
     # The method sets the interest gained for the account.
     def set_interest(self, interest):
